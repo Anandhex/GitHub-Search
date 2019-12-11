1d423e5eda9d99b7e7c9250ac3d6507c2b5c55e0
Anandhex <anand.patil.ap79@gmail.com>
2019-12-11 16:48:19 +0530

    Updates

---
+++ precache-manifest.88aacc736f080a1d5ccc47d140fd8a76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25b0d5a3db42b388f8e940461087e058",
+    "url": "/Anandhex/GitHub-Search/index.html"
+  },
+  {
+    "revision": "5c4b97d7f33f58fd4723",
+    "url": "/Anandhex/GitHub-Search/static/css/main.933fcfd2.chunk.css"
+  },
+  {
+    "revision": "de83ccfc3a23570a04c4",
+    "url": "/Anandhex/GitHub-Search/static/js/2.df70fc35.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/Anandhex/GitHub-Search/static/js/2.df70fc35.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5c4b97d7f33f58fd4723",
+    "url": "/Anandhex/GitHub-Search/static/js/main.a8258571.chunk.js"
+  },
+  {
+    "revision": "1c301cf7070fa296dd63",
+    "url": "/Anandhex/GitHub-Search/static/js/runtime-main.127e520d.js"
+  }
+]);
